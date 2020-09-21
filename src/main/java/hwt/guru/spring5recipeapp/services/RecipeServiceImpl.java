a6b8440cf1742117cf10d111c9c5da84379da265
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-22 00:20:21 +0800

    Using Project Lombok

--- src/main/java/hwt/guru/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/hwt/guru/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package hwt.guru.spring5recipeapp.services;
 
 import hwt.guru.spring5recipeapp.model.Recipe;
 import hwt.guru.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("Hey, I'm in the service now");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
