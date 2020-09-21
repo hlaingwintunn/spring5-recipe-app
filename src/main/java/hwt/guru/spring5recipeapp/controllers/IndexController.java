bc344578e894e85fe8da1c0d256a9b6af258cca8
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-21 12:17:40 +0800

    Spring Data JPA query method.

--- src/main/java/hwt/guru/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/hwt/guru/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package hwt.guru.spring5recipeapp.controllers;
 
+import hwt.guru.spring5recipeapp.model.Category;
+import hwt.guru.spring5recipeapp.model.UnitOfMeasure;
+import hwt.guru.spring5recipeapp.repositories.CategoryRepository;
+import hwt.guru.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByUom("Tablespoon");
+
+        System.out.println("Category Id is : " + category.get().getId());
+        System.out.println("UOM Id is : " + uom.get().getId());
+
         return "index";
     }
 }
--- src/main/java/hwt/guru/spring5recipeapp/model/UnitOfMeasure.java
+++ src/main/java/hwt/guru/spring5recipeapp/model/UnitOfMeasure.java
@@ -7,9 +7,17 @@ public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUom() {
         return uom;
