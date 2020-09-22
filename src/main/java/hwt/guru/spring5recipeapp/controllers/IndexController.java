3ea37c5fdbc6a3eb302e5ec0cf82636cc0bb6a3a
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-22 23:51:44 +0800

    Using Lombok : assignment

--- src/main/java/hwt/guru/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/hwt/guru/spring5recipeapp/controllers/IndexController.java
@@ -3,12 +3,14 @@ package hwt.guru.spring5recipeapp.controllers;
 import hwt.guru.spring5recipeapp.model.Category;
 import hwt.guru.spring5recipeapp.model.UnitOfMeasure;
 import hwt.guru.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -19,7 +21,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
-
+        log.debug("Index Page Controller");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
--- src/main/java/hwt/guru/spring5recipeapp/model/Ingredient.java
+++ src/main/java/hwt/guru/spring5recipeapp/model/Ingredient.java
@@ -1,10 +1,12 @@
 package hwt.guru.spring5recipeapp.model;
 
 import ch.qos.logback.classic.boolex.GEventEvaluator;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Ingredient {
 
@@ -20,57 +22,4 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
-
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-    }
-
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-        this.recipe = recipe;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
 }
