53a223f75f80767570545e23bf97102fbfda7d97
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-25 00:22:49 +0800

    Spring Integration Test Intro

---
+++ src/test/java/hwt/guru/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,39 @@
+package hwt.guru.spring5recipeapp.repositories;
+
+import hwt.guru.spring5recipeapp.model.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> UomOpt = repository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", UomOpt.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() {
+        Optional<UnitOfMeasure> UomOpt = repository.findByDescription("Cup");
+
+        assertEquals("Cup", UomOpt.get().getDescription());
+    }
+}
