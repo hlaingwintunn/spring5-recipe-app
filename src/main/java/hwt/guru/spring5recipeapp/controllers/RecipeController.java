215aedeb980db86ee811bad6e5b48a567f6722ff
hlaingwintunn <hlaingwintunn@gmail.com>
2020-10-03 18:22:37 +0800

    create new Recipe

--- src/main/java/hwt/guru/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/hwt/guru/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package hwt.guru.spring5recipeapp.controllers;
 
+import hwt.guru.spring5recipeapp.commands.RecipeCommand;
 import hwt.guru.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,17 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand recipeCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + recipeCommand.getId();
     }
 }
