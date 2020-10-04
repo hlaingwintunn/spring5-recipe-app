e9f116b6bee1e844f0d84ec836b7761e3f25df18
hlaingwintunn <hlaingwintunn@gmail.com>
2020-10-04 18:05:37 +0800

    adding image upload - front end

---
+++ src/test/java/hwt/guru/spring5recipeapp/services/ImageServiceImplTest.java
@@ -0,0 +1,17 @@
+package hwt.guru.spring5recipeapp.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ImageServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void saveImageFile() {
+    }
+}
