3f8a2b17050582a880b30e193e03f3fa7456ab4e
hlaingwintunn <hlaingwintunn@gmail.com>
2020-10-11 20:34:09 +0800

    Using Spring MVC Annotation ResponseStatus

--- src/test/java/hwt/guru/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/hwt/guru/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -1,6 +1,7 @@
 package hwt.guru.spring5recipeapp.controllers;
 
 import hwt.guru.spring5recipeapp.commands.RecipeCommand;
+import hwt.guru.spring5recipeapp.exceptions.NotFoundException;
 import hwt.guru.spring5recipeapp.model.Recipe;
 import hwt.guru.spring5recipeapp.services.RecipeService;
 import org.junit.Before;
@@ -47,6 +48,14 @@ public class RecipeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    public void testGetRecipeNotFound() throws Exception {
+        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
